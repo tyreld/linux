708384f7da235df76aaf2c79e9dc41766cb45c8a
Tyrel Datwyler <tyreld@linux.vnet.ibm.com>
2017-04-20 17:59:43 -0400

    of/pseries: acquire of_root directly

--- arch/powerpc/platforms/pseries/setup.c
+++ arch/powerpc/platforms/pseries/setup.c
@@ -434,7 +434,7 @@ static void __init find_and_init_phbs(void)
 {
 	struct device_node *node;
 	struct pci_controller *phb;
-	struct device_node *root = of_find_node_by_path("/");
+	struct device_node *root = of_node_get(of_root);
 
 	for_each_child_of_node(root, node) {
 		if (node->type == NULL || (strcmp(node->type, "pci") != 0 &&
